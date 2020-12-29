b176d27bbad57b1a37c49da9768feaccf8078316
Apratim <apra487@gmail.com>
2020-12-29 09:34:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,18 +38,4 @@ const dayPrices = {};
 
 
 
-// ! Extra tool
-/*
-function url(x){
-    x= x.toLowerCase().replace(' ', '-');
-    return `https://in.investing.com/equities/${x}`
-}
- console.log(stocks[i].ticker);
-    for (let i = 0; i < stocks.length; i++) {
-        let row = url(stocks[i].ticker);
-        stocks[i].url = row;
-        await stocks[i].save(); 
-    }
-    let header = ["date", "SBI", "Reliance Industries", "AXIS Bank", "ICICI Bank", "HDFC Bank", "Tata Motors", "Housing Development Finance", "IndusInd Bank", "Indian Oil", "Tata Steel Ltd", "Kotak Mahindra Bank", "ITC", "Federal Bank", "Hindustan Unilever", "Tata Consultancy", "Wipro", "Bharti Airtel", "Infosys", "Oil & Natural Gas", "Piramal Enterprises", "UPL", "Sun Pharma", "IDFC First Bank", "Alok Industries", "Britannia Industries", "Punjab National Bank", "Ashok Leyland", "Vodafone Idea", "Yes Bank", "Bank Of Baroda", "Maruti Suzuki", "RBL Bank", "Titan Company", "Carnation Ind", "Panth Infinity", "Colgate-Palmolive India", "Larsen & Toubro", "Asian Paints", "Bajaj Finance", "Tata Chemicals", "Nestle India", "Tara Chand Logistic", "ACC", "Bajaj Finserv", "Bharat Petroleum", "Indiabulls", "Mahindra & Mahindra", "Bajaj Auto", "Motherson Sumi Systems", "Steel Authority"];
-    await sheet.setHeaderRow(header, 1);
-*/
+
